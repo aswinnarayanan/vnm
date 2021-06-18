b6c1dd930fbb2ab925711a92625f05ce1de4f7f3
Steffen Bollmann <stebo85@users.noreply.github.com>
2021-06-18 20:41:38 +1000

    adjusted screen size in build script windows

--- build_run_stop.bat
+++ build_run_stop.bat
@@ -1,9 +1,9 @@
-docker build -t vnm:latest . && (
+docker build -t vnm:latest . --file vnm_base/Dockerfile && (
     ECHO "Starting VNM:"
     docker images
     REM docker run -d --privileged --name vnm -v C:/vnm:/vnm -p 6080:80 -p 5900:5900 vnm:latest
     REM docker run -d --privileged --name vnm -v C:/vnm:/vnm -e RESOLUTION=1920x990 -p 6080:80 -p 5900:5900 vnm:latest
-    docker run -d --privileged --name vnm -v C:/vnm:/vnm -e RESOLUTION=1280x680 -e USER=neuro -p 6080:80 -p 5900:5900 vnm:latest
+    docker run -d --privileged --name vnm -v C:/vnm:/vnm -e RESOLUTION=1600x960 -e USER=neuro -p 6080:80 -p 5900:5900 vnm:latest
     set /p=VNM is running - press ENTER key to shutdown and quit VNM!
     docker stop vnm
     docker rm vnm
